1dd62a8a0fd583fe6b24df56865ca1b79ea67ea4
Milk Midav <wadim017@gmail.com>
2022-12-03 23:29:16 +0200

    added 1440px style for page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl-custom': '1440px',
+      // => @media (min-width: 1440px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         body:['Manrope']
@@ -9,7 +28,7 @@ module.exports = {
       gridTemplateColumns: {
         'custom': 'repeat(4, minmax(0, 176px))',
         'custom-2cols': 'repeat(2, minmax(0, 316px))',
-
+        'custom-3cols': 'repeat(3, minmax(0, 316px))',
       },
     },
   },
